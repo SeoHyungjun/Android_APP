e617c824cc8196faf989408e8bf6a9f9a9f6dd8c
SeoHyungjun <wewwe96@naver.com>
2021-10-26 20:40:25 +0900

    feat : add and set layout id
    add id of layout of activity_main.xml
    - add id of EditText and Button
    set value of MainActivity.kt from id of xml file
    - set id value of layout

--- BMI_calculator/app/src/main/java/hyungjun/seo/app/bmicalculator/MainActivity.kt
+++ BMI_calculator/app/src/main/java/hyungjun/seo/app/bmicalculator/MainActivity.kt
@@ -2,10 +2,19 @@ package hyungjun.seo.app.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 아래 경우에는 heightEditText의 타입이 EditText라고 명시
+        val heightEditText: EditText = findViewById(R.id.heightEditText)
+        // 아래 경우에는 weightEditText의 타입을 명시하지 않고, findViewById의 return값이 EditText라고 명시
+        val weightEditText = findViewById<EditText>(R.id.weightEditText)
+
+        val resultButton = findViewById<Button>(R.id.resultButton)
     }
 }
