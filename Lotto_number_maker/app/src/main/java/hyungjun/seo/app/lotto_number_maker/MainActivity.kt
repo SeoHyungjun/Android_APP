d404c41cc1772a04039a45f175554ae9e5679ac4
SeoHyungjun <wewwe96@naver.com>
2021-10-28 22:00:52 +0900

    feat : init lotto number maker and add basic layout

---
+++ Lotto_number_maker/app/src/main/java/hyungjun/seo/app/lotto_number_maker/MainActivity.kt
@@ -0,0 +1,11 @@
+package hyungjun.seo.app.lotto_number_maker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
