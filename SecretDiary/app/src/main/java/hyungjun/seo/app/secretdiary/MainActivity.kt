f8a515d3bc13a7a6793fb4c066f04752651be129
SeoHyungjun <wewwe96@naver.com>
2021-11-04 21:50:28 +0900

    feat : init Secret Diary and add basic layout
    Initial layout settings
       - Added 2 buttons and 3 number pickers
       - Change the background color
       - Added The Secret Garden text
    Change settings
       - Add fonts to the res/style/font directory
       - Add NoActionBar theme to res/values/themes.xml

---
+++ SecretDiary/app/src/main/java/hyungjun/seo/app/secretdiary/MainActivity.kt
@@ -0,0 +1,12 @@
+
+package hyungjun.seo.app.secretdiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
