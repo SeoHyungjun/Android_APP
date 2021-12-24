18b794c1befcaf3057d2c04a9965b5133473fbbe
SeoHyungjun <wewwe96@naver.com>
2021-12-24 20:16:23 +0900

    FEAT: Added Timer and function to change time according to SeekBar
    SeekBar.setOnSeekBarCahgneListener
         onProgressChanged
             Update time only when the user directly manipulates the SeekBar through fromUser
         onStartTrackingTouch
             Cancel Timer when SeekBar is touched
         onStopTrackingTouch
             When the SeekBar operation is finished, a new Timer is created by changing the SeekBar's Progress value to milliseconds.

--- Pomodoro_Timer/app/src/main/java/hyungjun/seo/app/pomodoro_timer/MainActivity.kt
+++ Pomodoro_Timer/app/src/main/java/hyungjun/seo/app/pomodoro_timer/MainActivity.kt
@@ -3,6 +3,7 @@ package hyungjun.seo.app.pomodoro_timer
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.SeekBar
 import android.widget.TextView
 import org.w3c.dom.Text
@@ -21,6 +22,8 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.seekBar)
     }
 
+    private var currentCountDownTimer: CountDownTimer?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,20 +34,51 @@ class MainActivity : AppCompatActivity() {
     private fun bindViews() {
         seekBar.setOnSeekBarChangeListener(
             object : SeekBar.OnSeekBarChangeListener {
-                @SuppressLint("SetTextI18n")
                 override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                    remainMinutesTextView.text = "%02d.format(progress)"
+                    if (fromUser){
+                        updateRemainTimes(progress * 60 * 1000L)
+                    }
                 }
 
                 override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                    TODO("Not yet implemented")
+                    currentCountDownTimer?.cancel()
+                    currentCountDownTimer = null
                 }
 
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                    TODO("Not yet implemented")
+                    seekBar ?: return
+
+                    currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
+                    currentCountDownTimer?.start()
                 }
             }
         )
+    }
+
+    private fun createCountDownTimer(initialMillis: Long): CountDownTimer {
+        return object: CountDownTimer(initialMillis, 1000L){
+            override fun onTick(millisUntilFinished: Long) {
+                updateRemainTimes(millisUntilFinished)
+                updateSeekBar(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                updateRemainTimes(0)
+                updateSeekBar(0)
+            }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateRemainTimes(remainMillis: Long) {
+        val remainSeconds = remainMillis / 1000
+
+        remainMinutesTextView.text = "%02d".format(remainSeconds / 60)
+        remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
+    }
+
+    private fun updateSeekBar(remainMillis: Long) {
+        seekBar.progress = (remainMillis / 1000 / 60).toInt()
     }
 
 }
